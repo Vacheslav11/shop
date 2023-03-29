b323cec7c7df5a6dbc354004ad8c9f0e932ef96b
Vachesav11 <shadowsrespektq@gmail.com>
2023-03-29 19:02:43 +0300

    add phone css

--- done.js
+++ done.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const button = document.querySelector('.menu__mobile_button');
+const menu = document.getElementById( 'menu__mobile-text')
 
 button.addEventListener('click', () => {
     button.classList.toggle('active');
-})
+    menu.classList.toggle( 'show')
+});
